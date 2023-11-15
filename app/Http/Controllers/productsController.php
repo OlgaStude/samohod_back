ed8d8a3c45a132e6d60041c90cd24e3365a10885
OlgaStude <112942851+OlgaStude@users.noreply.github.com>
2023-11-15 13:49:04 +0400

    Add files via upload

--- app/Http/Controllers/productsController.php
+++ app/Http/Controllers/productsController.php
@@ -8,13 +8,12 @@ use Illuminate\Http\Request;
 
 class productsController extends Controller
 {
-    
-    public function show(){
 
-        $products = ['content' =>  productRecourse::collection(Product::all())];
+    public function show()
+    {
+
+        $products = ['content' =>  productRecourse::collection(Product::all()), 'what' => 'is'];
 
         return response($products, 200)->header('Status', '200');
-
     }
-
 }
